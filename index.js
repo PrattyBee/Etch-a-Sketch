ac05e78fb44aabc609a5f673b97716fc27b1e92f
PrattyBee <pratyush.kaintura@hotmail.com>
2023-06-07 13:15:11 -0400

    add button to delete squares

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector("#grid");
-
+const button = document.querySelector('#btn');
 
 for (let i = 1; i < 17; i++) {
   let div = document.createElement("div");
@@ -7,3 +7,13 @@ for (let i = 1; i < 17; i++) {
   div.classList.add('square');
   grid.appendChild(div);
 }
+
+function removeDivs() {
+    let divs = document.querySelectorAll('.square');
+    divs = Array.from(divs);
+    divs.forEach((element) => {
+        element.remove();
+    })
+}
+
+button.addEventListener('click', removeDivs);
